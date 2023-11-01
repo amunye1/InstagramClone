e8f964b69105a1b6cdce2ad713c015a91487d4a2
abdullahimunye <amunye1@ggc.edu>
2023-11-01 15:31:55 -0400

    working on favourite screen . specifically trying to fix the list of images with the text and button

--- app/src/main/java/com/vipulasri/jetinstagram/ui/favorite/Favorites.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/favorite/Favorites.kt
@@ -2,8 +2,10 @@ package com.vipulasri.jetinstagram.ui.favorite
 
 import androidx.compose.compiler.plugins.kotlin.ComposeFqNames.remember
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,15 +15,20 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontStyle
@@ -29,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vipulasri.jetinstagram.R
+import com.vipulasri.jetinstagram.data.StoriesRepository
 import com.vipulasri.jetinstagram.ui.components.icon
 
 @ExperimentalFoundationApi
@@ -36,15 +44,20 @@ import com.vipulasri.jetinstagram.ui.components.icon
 fun FavoriteScreen(modifier: Modifier = Modifier) {
 
 
-
-    Scaffold(
-        topBar = { Toolbar() }){
+    val stories by StoriesRepository.observeStories()
+    Scaffold(topBar = { Toolbar() }){
 
         Spacer(modifier = Modifier.padding(20.dp))
-        Text(text = stringResource(R.string.favourite_text)
-            , fontSize = 16.sp,
-            modifier = Modifier.padding(16.dp)
+
+        Column {
+            Text(text = stringResource(R.string.favourite_text)
+                , fontSize = 16.sp,
+                modifier = Modifier.padding(16.dp)
             )
+            SearchTextField()
+
+            StoriesSection(stories = stories)
+        }
 
     }
 }
@@ -88,11 +101,32 @@ private fun Toolbar() {
 @Composable
 fun SearchTextField() {
     val searchText = remember { mutableStateOf("") }
+    val searchIcon: Painter = painterResource(id = R.drawable.ic_search)
 
+    val customTextFieldColors = TextFieldDefaults.textFieldColors(
+        backgroundColor = Color.White, // Set the background color to white
+        textColor = Color.Gray, // Set the text color to gray
+        focusedIndicatorColor = Color.Transparent, // Set focused indicator color to transparent
+        unfocusedIndicatorColor = Color.Transparent, // Set unfocused indicator color to transparent
+        cursorColor = Color.Gray // Set cursor color to gray
+    )
     TextField(
         value = searchText.value,
         onValueChange = { newValue ->
             searchText.value = newValue
-        }
+        },
+        modifier = Modifier.fillMaxWidth(),
+        label = {
+            Text(text = "Search")
+        },
+
+                leadingIcon = {
+            Image(
+                painter = searchIcon,
+                contentDescription = "this is a search icon" // Provide a content description for accessibility
+            )
+        },
+        colors = customTextFieldColors
+
     )
 }
--- app/src/main/java/com/vipulasri/jetinstagram/ui/favorite/FavoritesList.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/favorite/FavoritesList.kt
@@ -1,2 +1,101 @@
 package com.vipulasri.jetinstagram.ui.favorite
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Colors
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.vipulasri.jetinstagram.model.Story
+import com.vipulasri.jetinstagram.ui.home.StoryImage
+
+
+@Composable
+ fun StoriesSection(stories: List<Story>) {
+    Column {
+        StoriesList(stories)
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
+
+@Composable
+private fun StoriesList(stories: List<Story>) {
+    LazyColumn {
+        itemsIndexed(stories) { index, story ->
+
+            if (index == 0) {
+                Spacer(modifier = Modifier.width(6.dp))
+            }
+            Row (modifier = Modifier.fillMaxWidth()
+                ,
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(vertical = 5.dp, horizontal = 6.dp)
+
+                ) {
+
+                    StoryImage(imageUrl = story.image)
+                    Spacer(modifier = Modifier.height(5.dp))
+
+
+                }
+
+                if (index == stories.size.minus(1)) {
+                    Spacer(modifier = Modifier.width(6.dp))
+                }
+                Text(story.name, style = MaterialTheme.typography.caption)
+                PurpleAddButton()
+            }
+
+
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PurpleAddButton() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), // Adjust padding as needed
+        shape = RoundedCornerShape(8.dp), // Adjust corner radius as needed
+        color = MaterialTheme.colors.primary, // Set the background color to purple
+        onClick = {
+            // Handle button click action here
+        }
+    ) {
+
+        Text(
+            text = "Add",
+            modifier = Modifier
+                .padding(8.dp), // Adjust padding within the button as needed
+            style = MaterialTheme.typography.button.copy(
+                color = Color.White // Set the text color to white
+            )
+        )
+    }
+}
+
